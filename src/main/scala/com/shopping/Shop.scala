468afb186263506c79c4e4da1f761de7da0b0afd
shettyshrikanth <skrshetty@hotmail.com>
2016-06-11 14:50:44 +0100

    Test and Logic added for checout system

--- src/main/scala/com/shopping/Shop.scala
+++ src/main/scala/com/shopping/Shop.scala
@@ -1,8 +1,12 @@
 package com.shopping
 
 object Shop {
-
+ 
   def apply(items: Fruit*): Seq[Fruit] = {
     items.toSeq    
+  }
+
+  def checkout(shoppingCart: Fruit *) : BigDecimal = {
+    shoppingCart map (item => item.price) sum
   }
 }
--- src/test/scala/com/shopping/ShopTest.scala
+++ src/test/scala/com/shopping/ShopTest.scala
@@ -5,27 +5,38 @@ import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class ShopTest  extends Specification   {
-  
+class ShopTest extends Specification {
+
   "Shop which only sells apples and oranges " should {
     "Sell only Apples And Oranges" in {
-       val size = Shop(Apple, Orange) filter { case item @ (Apple | Orange) => false; case _ => false} size 
-       
-       size mustEqual 0
-    }         
+      val size = Shop(Apple, Orange) filter { case item @ (Apple | Orange) => false; case _ => false } size
+
+      size mustEqual 0
+    }
   }
-  
+
   "Apples cost 60p and Oranges cost 25p, therefore shop " should {
     "Sell Apple for 60p each " in {
-      val appleList = Shop(Apple, Orange) filter { case item @ Apple => true; case _ => false } 
-      
+      val appleList = Shop(Apple, Orange) filter { case item @ Apple => true; case _ => false }
+
       appleList(0).price mustEqual 0.60
     }
-    
+
     "Sell Orange for 25p each " in {
-      val orangeList = Shop(Apple, Orange) filter { case item @ Orange => true; case _ => false } 
-      
+      val orangeList = Shop(Apple, Orange) filter { case item @ Orange => true; case _ => false }
+
       orangeList(0).price mustEqual 0.25
-    }    
+    }
+  }
+
+  "Checkout system which takes a list of items scanned at the till and outputs the total cost" should {
+
+    "3 Apples and 1 Orange cost \\£2.05  " in {
+      Shop.checkout(Apple, Apple, Apple, Orange) mustEqual 2.05
+    }
+    
+    " Empty shopping cart cost 0.00 " in {
+      Shop.checkout() mustEqual 0.0       
+    }
   }
 }
